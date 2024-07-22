c170be06b74f944f328dba5a1e73d3344853b45e
unknown <Louis Pierre Andre>
2024-07-22 06:49:31 -0400

    install React app and tailwind css

---
+++ my_blog/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1 className='p-3'>Welcom to Vite AProject</h1>
+   </div>
+  )
+}
+
+export default App
